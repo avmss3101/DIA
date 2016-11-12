c5a73bedd19d67cb9be7c7c543d131fb93684cfe
André Viana Martins Silveira de Souza <andresilveira1992@gmail.com>
2016-11-12 13:17:42 -0200

    Add files via upload

---
+++ val12851.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <stdio.h>
+#include <math.h>
+
+#define PI acos(-1);
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+	int t, c;
+	double d, v, diam;
+	c = 1;
+	cin >> t;
+	
+	while (t--) {
+		cin >> d;
+		cin >> v;
+		
+		v = v * 282.0;
+		d = d * PI;
+		
+		v = v*3/d;
+		
+		diam = sqrt(v*16.0/7.0);
+		
+		printf ("Case %d: %.3f\n", c, diam);
+		c++;
+	}
+	
+	return 0;
+}
+
